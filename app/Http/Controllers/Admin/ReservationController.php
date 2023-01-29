228711db10208bfb181e3a4bffcedeab766dc206
AldiZulfikar <aldizulfikar04@gmail.com>
2023-01-29 15:49:37 +0700

    flash alert message

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -43,7 +43,7 @@ class ReservationController extends Controller
     {
         Reservation::create($request->validated());
 
-        return to_route("admin.reservation.index");
+        return to_route("admin.reservation.index")->with('success', 'Reservation created successfully!');
     }
 
     /**
--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -45,7 +45,7 @@ class TableController extends Controller
             'location' => $request->location,
         ]);
 
-        return to_route('admin.tables.index');
+        return to_route('admin.tables.index')->with('success', 'Table created successfully!');
     }
 
     /**
@@ -70,7 +70,7 @@ class TableController extends Controller
     {
         $table->update($request->validated());
 
-        return to_route('admin.tables.index');
+        return to_route('admin.tables.index')->with('warning', 'Table updated successfully!');
     }
 
     /**
@@ -81,8 +81,9 @@ class TableController extends Controller
      */
     public function destroy(Table $table)
     {
+        $table -> reservation()->delete();
         $table -> delete();
 
-        return to_route('admin.tables.index');
+        return to_route('admin.tables.index')->with('danger', 'Table deleted successfully!');
     }
 }
